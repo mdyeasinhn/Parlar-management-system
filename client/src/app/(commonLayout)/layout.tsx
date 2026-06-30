95f2db5b2ce4bba2baccb8b18a9508e3644057fa
Md Yeasin Arafat <mdyeasinhn@gmail.com>
2026-06-30 22:44:55 +0600

    feat: update layout with Footer component, add ServiceSection, and refactor Banner import; update dependencies

--- client/src/app/(commonLayout)/layout.tsx
+++ client/src/app/(commonLayout)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/shared/Footer';
 import Navbar from '@/components/shared/Navbar';
 import React from 'react';
 
@@ -6,7 +7,7 @@ const CommonLayout = ({children} : {children: React.ReactNode}) => {
         <div>
         <Navbar/>
             {children}
-            
+            <Footer/>
         </div>
     );
 };
