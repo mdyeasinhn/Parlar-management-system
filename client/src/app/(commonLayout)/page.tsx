063d38db5afbe05a030f57a9dfb2ed6a8a953328
Md Yeasin Arafat <mdyeasinhn@gmail.com>
2026-07-01 22:21:22 +0600

    feat: add Testimonials and ContactForm components; update HomePage layout to include new sections

--- client/src/app/(commonLayout)/page.tsx
+++ client/src/app/(commonLayout)/page.tsx
@@ -1,11 +1,15 @@
 import Banner from '@/components/home/Banner';
+import ContactForm from '@/components/home/contact';
 import ServiceSection from '@/components/home/services';
+import Testimonials from '@/components/home/Testimonials';
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
       <ServiceSection/>
+      <Testimonials/>
+      <ContactForm/>
     </div>
   );
 };
